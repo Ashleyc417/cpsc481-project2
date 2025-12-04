d03bccffb4e6e90fe96a3c7c1c6fa9d0b0671108
elizabethmazuca <elizabethmazuca@csu.fullerton.edu>
2025-12-04 03:06:58 -0800

    Implement ai move selection using minimax

--- src/lib/recommender/getBestMove.ts
+++ src/lib/recommender/getBestMove.ts
@@ -1,14 +1,14 @@
 import { ROWS } from '$lib/connect4/logic';
+import { minimax } from './minimax';
 
 export function getBestMove(board: number[][], player: number): [number, number] {
-    const move = 4;
+    const depth = 6;
+    const [, move] = minimax(board, depth, -Infinity, Infinity, true, player);
+    if (!move) return [-1, -1];
     for (let r = ROWS - 1; r >= 0; r--) {
         if (board[r][move] === 0) {
             return [r, move];
         }
     }
     return [-1, -1];
-    // const depth = 6;
-    // const [, move] = minimax(board, depth, -Infinity, Infinity, true, player);
-    // return move ?? 0;
 }
