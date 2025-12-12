1620b3413b3a5e8e84a7208587d644520b301961
Tomas Oh <tomasoh@csu.fullerton.edu>
2025-12-12 13:38:30 -0800

    remove the infamous [-1, -1] move

--- src/lib/recommender/getBestMove.ts
+++ src/lib/recommender/getBestMove.ts
@@ -1,14 +1,14 @@
 import { ROWS } from '$lib/connect4/logic';
 import { minimax } from './minimax';
 
-export function getBestMove(board: number[][], player: number): [number, number] {
+export function getBestMove(board: number[][], player: number): [number, number] | null {
 	const depth = 6;
 	const [, move] = minimax(board, depth, -Infinity, Infinity, true, player);
-	if (!move) return [-1, -1];
+	if (!move) return null;
 	for (let r = ROWS - 1; r >= 0; r--) {
 		if (board[r][move] === 0) {
 			return [r, move];
 		}
 	}
-	return [-1, -1];
+	throw new Error('Attempting to recommend an invalid move.');
 }
