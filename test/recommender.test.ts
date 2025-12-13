d26691f951a30056d6c2c2ce054b7c6efcdf24dd
Tomas Oh <tomasoh@csu.fullerton.edu>
2025-12-12 16:37:19 -0800

    run test cases 100 times to measure perf

--- test/recommender.test.ts
+++ test/recommender.test.ts
@@ -3,32 +3,44 @@ import boardData from './board-data.json';
 import { minimax } from '../src/lib/recommender/minimax';
 
 const SEARCH_DEPTH = 6;
+const NUM_ITERATIONS = 100;
+const TEST_RUNNING_TIME = 10 * 1000;
 
 describe('Connect 4 AI minimax evaluation', () => {
 	for (const test of boardData.tests) {
-		it(test.name, () => {
-			const start = performance.now();
-			const [score, move] = minimax(
-				test.board,
-				SEARCH_DEPTH,
-				-Infinity,
-				Infinity,
-				true,
-				test.player
-			);
-			const end = performance.now();
-			const time = (end - start).toFixed(2);
+		it(
+			test.name,
+			() => {
+				let totalTime = 0;
+				let score = null;
+				let move = null;
 
-			console.log(`Name: ${test.name}`);
-			console.log(`Description: ${test.description}`);
-			console.log(`Player's Turn: ${test.player}`);
-			console.log(`Selected Move: ${move}`);
-			console.log(`Score: ${score}`);
-			console.log(`Time: ${time}ms`);
+				for (let i = 0; i < NUM_ITERATIONS; i++) {
+					const start = performance.now();
+					const [s, m] = minimax(test.board, SEARCH_DEPTH, -Infinity, Infinity, true, test.player);
+					const end = performance.now();
+					expect(m).not.toBeNull();
+					expect(m).toBeGreaterThanOrEqual(0);
+					expect(m).toBeLessThan(7);
+					if (score === null || move === null) {
+						score = s;
+						move = m;
+					} else {
+						expect(m).toEqual(move);
+						expect(s).toEqual(score);
+					}
+					totalTime += end - start;
+				}
+				const avgTime = (totalTime / NUM_ITERATIONS).toFixed(2);
 
-			expect(move).not.toBeNull();
-			expect(move).toBeGreaterThanOrEqual(0);
-			expect(move).toBeLessThan(7);
-		});
+				console.log(`Name: ${test.name}`);
+				console.log(`Description: ${test.description}`);
+				console.log(`Player's Turn: ${test.player}`);
+				console.log(`Selected Move: ${move}`);
+				console.log(`Score: ${score}`);
+				console.log(`Time: ${avgTime}ms`);
+			},
+			TEST_RUNNING_TIME
+		);
 	}
 });
